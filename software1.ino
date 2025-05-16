8a3ec34ed99d184fde192e9ca7de5a313c3660e1
Siang <lego3517@gmail.com>
2025-05-17 00:30:03 +0800

    2nd commit

--- software1.ino
+++ software1.ino
@@ -36,9 +36,90 @@ const byte digitSegments[10][7] = {
 };
 
 void setup() {
-  // put your setup code here, to run once:
+  // Pin modes for optical sensors
+  pinMode(ENTRY_SENSOR_1, INPUT_PULLUP);
+  pinMode(EXIT_SENSOR_1, INPUT_PULLUP);
+  pinMode(ENTRY_SENSOR_2, INPUT_PULLUP);
+  pinMode(EXIT_SENSOR_2, INPUT_PULLUP);
+
+  // Segment pins
+  pinMode(SEG_A, OUTPUT);
+  pinMode(SEG_B, OUTPUT);
+  pinMode(SEG_C, OUTPUT);
+  pinMode(SEG_D, OUTPUT);
+  pinMode(SEG_E, OUTPUT);
+  pinMode(SEG_F, OUTPUT);
+  pinMode(SEG_G, OUTPUT);
+
+  // Digit select pins
+  pinMode(DIGIT1, OUTPUT);
+  pinMode(DIGIT2, OUTPUT);
+
+  digitalWrite(DIGIT1, LOW);
+  digitalWrite(DIGIT2, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  handleSensors();
+  updateDisplay();
 }
+
+void handleSensors() {
+  bool currentEntry1 = digitalRead(ENTRY_SENSOR_1);
+  bool currentExit1 = digitalRead(EXIT_SENSOR_1);
+  bool currentEntry2 = digitalRead(ENTRY_SENSOR_2);
+  bool currentExit2 = digitalRead(EXIT_SENSOR_2);
+
+  if (lastEntry1 == HIGH && currentEntry1 == LOW && region1_spaces > 0) {
+    region1_spaces--;
+  }
+  if (lastExit1 == HIGH && currentExit1 == LOW && region1_spaces < max_spaces) {
+    region1_spaces++;
+  }
+  if (lastEntry2 == HIGH && currentEntry2 == LOW && region2_spaces > 0) {
+    region2_spaces--;
+  }
+  if (lastExit2 == HIGH && currentExit2 == LOW && region2_spaces < max_spaces) {
+    region2_spaces++;
+  }
+
+  lastEntry1 = currentEntry1;
+  lastExit1 = currentExit1;
+  lastEntry2 = currentEntry2;
+  lastExit2 = currentExit2;
+
+  delay(100);
+}
+
+void updateDisplay() {
+  int value = constrain(region1_spaces, 0, 99);
+  int tens = value / 10;
+  int ones = value % 10;
+
+  // Display tens digit
+  digitalWrite(DIGIT1, HIGH);
+  digitalWrite(DIGIT2, LOW);
+  setSegments(tens);
+  delay(5);
+
+  // Display ones digit
+  digitalWrite(DIGIT1, LOW);
+  digitalWrite(DIGIT2, HIGH);
+  setSegments(ones);
+  delay(5);
+
+  // Turn off both digits
+  digitalWrite(DIGIT1, LOW);
+  digitalWrite(DIGIT2, LOW);
+}
+
+void setSegments(int num) {
+  digitalWrite(SEG_A, digitSegments[num][0]);
+  digitalWrite(SEG_B, digitSegments[num][1]);
+  digitalWrite(SEG_C, digitSegments[num][2]);
+  digitalWrite(SEG_D, digitSegments[num][3]);
+  digitalWrite(SEG_E, digitSegments[num][4]);
+  digitalWrite(SEG_F, digitSegments[num][5]);
+  digitalWrite(SEG_G, digitSegments[num][6]);
+}
+
